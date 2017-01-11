0b2941061aad3469933b26985774337350db7ebb
Nairitya Khilari <nkmann2@gmail.com>
2017-01-11 16:49:09 +0530

    temp fix for issue #121

--- src/emailjs-imap-client-imap.js
+++ src/emailjs-imap-client-imap.js
@@ -303,7 +303,12 @@
         if (this.compressed) {
             this._sendCompressed(buffer);
         } else {
-            this.socket.send(buffer);
+            if(!this.socket){
+                this._onError(new Error("Error :: Unexpected socket close"));
+            }
+            else {
+                this.socket.send(buffer);
+            }
         }
     };
 
